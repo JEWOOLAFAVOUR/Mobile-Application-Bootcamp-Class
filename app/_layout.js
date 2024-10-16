e119eba2c2fea68b5ac6e9419490aadc90bf9c94
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2024-10-16 17:14:12 +0100

    sksk

--- app/_layout.js
+++ app/_layout.js
@@ -1,42 +1,43 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, FlatList } from 'react-native'
 import React from 'react'
 
 const _layout = () => {
+    const listData = [
+        {
+            alphabet: "A",
+            description: "Apple",
+        }, {
+            alphabet: "B",
+            description: "Ball",
+        }, {
+            alphabet: "C",
+            description: "Cat",
+        },
+    ];
     return (
         <View style={styles.page}>
-            <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
-                <View style={styles.box1}></View>
-                <View style={styles.box2}></View>
-                <View style={styles.box3}></View>
-            </View>
-            <Text>Hello Mercy</Text>
+            <Text style={{ fontSize: 30, color: "black", fontWeight: "bold" }}>FlatList Class</Text>
+            <FlatList
+                data={listData}
+                renderItem={({ item }) => {
+                    return (
+                        <View>
+                            <Text>{item.alphabet} for {item.description}</Text>
+                        </View>
+                    )
+                }}
+            />
         </View>
-    );
-};
+    )
+}
 
 export default _layout
 
 const styles = StyleSheet.create({
     page: {
         flex: 1,
-        backgroundColor: 'white',
-        paddingVertical: 50,
-        paddingHorizontal: 10,
-    },
-    box1: {
-        height: 300,
-        width: 70,
-        backgroundColor: 'blue',
-    },
-    box2: {
-        height: 70,
-        width: 70,
-        backgroundColor: 'tomato',
-        // borderRadius: 110,
-    },
-    box3: {
-        height: 70,
-        width: 70,
-        backgroundColor: 'black',
+        backgroundColor: "#ffffff",
+        paddingTop: 40,
+        paddingHorizontal: 20,
     },
 })
---
+++ test.js
@@ -0,0 +1,66 @@
+// DECLARE A VARIABLE 
+
+const name = "Favour";
+
+// ARRAY and OBJECTS 
+
+// OBJECTS 
+const yusufData = {
+    firstname: "Adebayo",
+    lastname: "Yusuf",
+    level: "200 Level",
+    dept: "CSC",
+    age: 30,
+    phone: "08051296282",
+    sex: "Male",
+    admissionYear: 2022,
+    address: "Behind Mr Nibbles Backery, eating bread as a hobby",
+    stateOfOrigin: "Osun",
+}
+
+console.log(yusufData.firstname)
+
+// ARRAYS
+
+const threeHunderLevelStudents = [
+    {
+        firstname: "Adebayo",
+        lastname: "Yusuf",
+        level: "200 Level",
+        dept: "CSC",
+        age: 30,
+        phone: "08051296282",
+        sex: "Male",
+        admissionYear: 2022,
+        address: "Behind Mr Nibbles Backery, eating bread as a hobby",
+        stateOfOrigin: "Osun",
+    },
+    {
+        firstname: "Jacob",
+        lastname: "Adebayo",
+        level: "200 Level",
+        dept: "CSC",
+        age: 30,
+        phone: "08051296282",
+        sex: "Male",
+        admissionYear: 2022,
+        address: "Behind Mr Nibbles Backery, eating bread as a hobby",
+        stateOfOrigin: "Osun",
+    },
+]
+
+// console.log(typeof threeHunderLevelStudents)
+
+const studentData = ['Jacob', 'Mercy', 'Yusuf', 20, { ade: "boy" }]
+
+// console.log(studentData[1])
+
+
+
+
+// const studentData = []
+
+
+
+
+
