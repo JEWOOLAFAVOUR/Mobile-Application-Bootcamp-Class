d5378d5f07d80957309cf2ead74bef04920e2074
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2024-10-18 17:52:47 +0100

    sjsjs

--- app/_layout.js
+++ app/_layout.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Button } from 'react-native'
+import { StyleSheet, Text, View, Button, Image } from 'react-native'
 import React from 'react'
 
 const _layout = () => {
@@ -6,6 +6,12 @@ const _layout = () => {
         <View style={styles.page}>
             <Text style={{ fontSize: 20, color: "black", fontWeight: 'bold' }}>React Native Components</Text>
             <Button title='Press me' color={"black"} />
+
+            <Image source={require('../assets/images/abraham.jpg')}
+                style={{ height: 200, width: 200, borderRadius: 200 }} />
+
+            <Image source={{ uri: "https://img.freepik.com/premium-photo/god-makes-covenant-with-sarah-sarah-will-have-children-bible-stories_942102-18913.jpg?w=740" }}
+                style={{ height: 200, width: 200, borderRadius: 200 }} />
         </View>
     )
 }
