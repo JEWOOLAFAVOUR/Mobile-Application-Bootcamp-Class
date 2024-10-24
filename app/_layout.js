f141777b8db4be6df3f764d4a945e27ca6f99236
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2024-10-24 17:39:21 +0100

    sjsjs

--- app/_layout.js
+++ app/_layout.js
@@ -1,32 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React, { useState } from 'react'
-import { TouchableOpacity } from 'react-native'
+import React from 'react'
 
 const _layout = () => {
 
-    let [name, setName] = useState(0)
-
-    // let name = 'Jacobian'
+    const student = ['Abraham', 'Abiodun'];
 
     return (
         <View style={styles.page}>
-            <Text style={{ fontSize: 25, color: 'black', fontWeight: 'bold' }}>State Management Class</Text>
-
-            <View style={{ marginTop: 20, marginBottom: 40 }}>
-                <Text style={{ fontSize: 16, color: "#000" }}>My name is {name}.</Text>
-                <Text style={{ fontSize: 16, color: "#000" }}>My name is {name}.</Text>
-                <Text style={{ fontSize: 16, color: "#000" }}>My name is {name}.</Text>
-                <Text style={{ fontSize: 16, color: "#000" }}>My name is {name}.</Text>
-                <Text style={{ fontSize: 16, color: "#000" }}>My name is {name}.</Text>
-                <Text style={{ fontSize: 16, color: "#000" }}>My name is {name}.</Text>
-                <Text style={{ fontSize: 16, color: "#000" }}>My name is {name}.</Text>
-                <Text style={{ fontSize: 16, color: "#000" }}>My name is {name}.</Text>
-                <Text style={{ fontSize: 16, color: "#000" }}>My name is {name}.</Text>
-                <Text style={{ fontSize: 16, color: "#000" }}>My name is {name}.</Text>
-            </View>
-            <TouchableOpacity onPress={() => setName(name + 1)} style={styles.container}>
-                <Text style={{ fontSize: 14, color: 'white', }}>Click Me</Text>
-            </TouchableOpacity>
+            <Text style={{ fontSize: 35, color: "black", fontWeight: 'bold' }}>FlatList Class</Text>
         </View>
     )
 }
@@ -36,15 +17,8 @@ export default _layout
 const styles = StyleSheet.create({
     page: {
         flex: 1,
-        backgroundColor: '#ffffff',
+        backgroundColor: "#ffffff",
         paddingHorizontal: 20,
-        paddingTop: 40,
+        paddingTop: 10,
     },
-    container: {
-        height: 50,
-        backgroundColor: "blue",
-        borderRadius: 5,
-        justifyContent: 'center',
-        alignItems: "center",
-    },
-});
+})
---
+++ app/abraham/class3.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const class3 = () => {
+    return (
+        <View>
+            <Text>class3</Text>
+        </View>
+    )
+}
+
+export default class3
+
+const styles = StyleSheet.create({})
