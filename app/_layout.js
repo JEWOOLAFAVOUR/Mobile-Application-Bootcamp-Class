ab84f9b5190b126ed996e4d59d77975894b6b9d1
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2024-10-25 16:45:39 +0100

    done done

--- app/_layout.js
+++ app/_layout.js
@@ -1,50 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React, { useState } from 'react'
-import { TouchableOpacity } from 'react-native'
+import React from 'react'
 
 const _layout = () => {
-
-    let [name, setName] = useState(0)
-
-    // let name = 'Jacobian'
-
     return (
-        <View style={styles.page}>
-            <Text style={{ fontSize: 25, color: 'black', fontWeight: 'bold' }}>State Management Class</Text>
-
-            <View style={{ marginTop: 20, marginBottom: 40 }}>
-                <Text style={{ fontSize: 16, color: "#000" }}>My name is {name}.</Text>
-                <Text style={{ fontSize: 16, color: "#000" }}>My name is {name}.</Text>
-                <Text style={{ fontSize: 16, color: "#000" }}>My name is {name}.</Text>
-                <Text style={{ fontSize: 16, color: "#000" }}>My name is {name}.</Text>
-                <Text style={{ fontSize: 16, color: "#000" }}>My name is {name}.</Text>
-                <Text style={{ fontSize: 16, color: "#000" }}>My name is {name}.</Text>
-                <Text style={{ fontSize: 16, color: "#000" }}>My name is {name}.</Text>
-                <Text style={{ fontSize: 16, color: "#000" }}>My name is {name}.</Text>
-                <Text style={{ fontSize: 16, color: "#000" }}>My name is {name}.</Text>
-                <Text style={{ fontSize: 16, color: "#000" }}>My name is {name}.</Text>
-            </View>
-            <TouchableOpacity onPress={() => setName(name + 1)} style={styles.container}>
-                <Text style={{ fontSize: 14, color: 'white', }}>Click Me</Text>
-            </TouchableOpacity>
+        <View>
+            <Text>_layout</Text>
         </View>
     )
 }
 
 export default _layout
 
-const styles = StyleSheet.create({
-    page: {
-        flex: 1,
-        backgroundColor: '#ffffff',
-        paddingHorizontal: 20,
-        paddingTop: 40,
-    },
-    container: {
-        height: 50,
-        backgroundColor: "blue",
-        borderRadius: 5,
-        justifyContent: 'center',
-        alignItems: "center",
-    },
-});
+const styles = StyleSheet.create({})
